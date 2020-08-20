b39ccdb3455f479a7c01a52c0f04991835877239
seungyongC <likerberi97@gmail.com>
2020-08-20 22:34:52 +0900

    mine

--- testP.py
+++ testP.py
@@ -1,13 +1,36 @@
-count = int(input())
-for i in range(count):
-    for j in range(count):
-        if( i + j >= count - 1):
+col, row = map(int, input().split())
+matrix = []
+answer = [[0] * col for i in range(row)]
+for i in range(row):
+    matrix.append(list(input()))
+
+for i in range(row):
+    for j in range(col):
+        if(matrix[i][j] == '*'):
+            if(i - 1 >= 0):
+                if(j - 1 >= 0):
+                    answer[i-1][j-1] = answer[i-1][j-1] + 1
+                if(j + 1 < col):
+                    answer[i-1][j+1] = answer[i-1][j+1] + 1
+                
+                answer[i-1][j] = answer[i-1][j] + 1
+            if(i + 1 < row):
+                if(j - 1 >= 0):
+                    answer[i+1][j-1] = answer[i+1][j-1] + 1
+                if(j + 1 < col):
+                    answer[i+1][j+1] = answer[i+1][j+1] + 1
+                
+                answer[i+1][j] = answer[i+1][j] + 1
+            
+            if(j - 1 >= 0):
+                answer[i][j-1] = answer[i][j-1] + 1
+            if(j + 1 < col):
+                answer[i][j+1] = answer[i][j+1] + 1
+
+for i in range(row):
+    for j in range(col):
+        if(matrix[i][j] == '*'):
             print('*', end='')
         else:
-            print(' ', end='')
-    for k in range(count - 1):
-        if( k < i ):
-            print('*', end='')
-        else:
-            print(' ', end='')
+            print(answer[i][j], end='')
     print()
